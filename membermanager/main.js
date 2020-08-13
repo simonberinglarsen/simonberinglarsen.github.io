a87d3a18730c2749670c1a33f15c78be97a4e548
Simon Bering Larsen <XSISE@orsted.dk>
2020-08-13 15:17:35 +0200

    .

--- membermanager/main.js
+++ membermanager/main.js
@@ -43,6 +43,25 @@ function getText(id) {
     return document.getElementById(id).value;
 }
 
+function edit(memid) {
+    const member = members.find(m => m.memID === memid);
+    document.getElementById('memId').value = member.memID;
+    document.getElementById('memName').value = member.memName;
+    document.getElementById('memDob').value = member.memDob;
+    document.getElementById('memAddress').value = member.memAddress;
+    document.getElementById('memMobile').value = member.memMobile;
+    document.getElementById('memAge').value = member.memAge;
+    updateApp();
+}
+
+function deleteMember(memid) {
+    const existingMember = members.find(m => m.memID === memid);
+    if (existingMember) {
+        members.splice(members.indexOf(existingMember), 1);
+    }
+    updateApp();
+}
+
 // renders the member table in html based on the javascript arrays
 function renderUserTable() {
     const mapToRow = (member) => `<tr>   
@@ -51,12 +70,14 @@ function renderUserTable() {
     <td>${member.memDob}</td>
     <td>${member.memAddress}</td>
     <td>${member.memMobile}</td>
-    <td>${member.memAge}</td></tr>`;
+    <td>${member.memAge}</td>
+    <td><button onclick="edit('${member.memID}')">edit</button><button onclick="deleteMember('${member.memID}')">del</button></td></tr>`;
     document.getElementById('userTable').tBodies[0].innerHTML = `${members.map(m => mapToRow(m)).join('')}`;
 }
 
 // updates the application - keeps track of what to show and what to hide. Also updating dynamic things based on application state (appState)
 function updateApp() {
+    members.sort((a, b) => a.memID < b.memID ? -1 : 1);
     isVisible('loginScreen', !appState.loggedInUser);
     isVisible('loginValidation', appState.invalidUser);
     isVisible('memberAdminScreen', appState.loggedInUser);
@@ -76,6 +97,10 @@ document.getElementById('btnLogout').onclick = function () {
 
 // adds a new member to the array by reading the input and creating a new object to be pushed
 document.getElementById('btnAdd').onclick = function () {
+    const existingMember = members.find(m => m.memID === getText('memId'));
+    if (existingMember) {
+        members.splice(members.indexOf(existingMember), 1);
+    }
     members.push({
         memID: getText('memId'),
         memName: getText('memName'),
@@ -84,18 +109,13 @@ document.getElementById('btnAdd').onclick = function () {
         memMobile: getText('memMobile'),
         memAge: getText('memAge'),
     });
+    updateApp();
 }
 
 // clears the fields by setting the value to empty string
 document.getElementById('btnClear').onclick = function () {
     ['memId', 'memName', 'memDob', 'memAddress', 'memMobile', 'memAge'].forEach(e => document.getElementById(e).value = '');
 }
-
-// refreshes the list
-document.getElementById('btnRefresh').onclick = function () {
-    updateApp();
-}
-
 
 // button to login. gets the username and password from the text boxes and see if there is a matching user in the global array.
 // in the end the application state is updated and the updateApp() method makes sure to hide/show the right stuff
