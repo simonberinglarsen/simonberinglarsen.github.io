b42dee22d91e8ad3672d4c0ac043bdcff0041cdb
Simon Bering Larsen <XSISE@orsted.dk>
2020-08-13 14:07:20 +0200

    .

--- membermanager/main.js
+++ membermanager/main.js
@@ -14,23 +14,54 @@ const members = [
         memAddress: "500 woodlands RD #01-11. S411000",
         memMobile: "98765432",
         memAge: 29,
+    },
+    {
+        memID: "m002",
+        memName: "john Doe",
+        memDob: "26 June 1985",
+        memAddress: "501 woodlands RD #01-11. S411000",
+        memMobile: "11765432",
+        memAge: 31,
     }
+
 ];
 
 const appState = {
     loggedInUser: null,
-    
+
 };
 
 function isVisible(id, show) {
     document.getElementById(id).setAttribute("style", show ? "" : "display:none")
 }
 
+function renderUserTable() {
+    const mapToRow  = (member) => `<tr>   
+    <th>${member.memID}</th>
+    <td>${member.name}</td>
+    <td>${member.memDob}</td>
+    <td>${member.memAddress}</td>
+    <td>${member.memMobile}</td>
+    <td>${member.memAge}</td></tr>`;
+
+    document.getElementById('userTable').tBodies[0].innerHTML = `${members.map(m => mapToRow(m)).join('')}`;
+    
+}
+
 function updateApp() {
     isVisible('loginScreen', !appState.loggedInUser);
     isVisible('loginValidation', appState.invalidUser);
-    
     isVisible('memberAdminScreen', appState.loggedInUser);
+    if (appState.loggedInUser) {
+        renderUserTable();
+    }
+
+}
+
+document.getElementById('btnLogout').onclick = function () {
+    appState.loggedInUser = null;
+    appState.invalidUser = false;
+    updateApp();
 }
 
 document.getElementById('btnLogin').onclick = function () {
@@ -47,5 +78,5 @@ document.getElementById('btnLogin').onclick = function () {
     updateApp();
 }
 
-
+appState.loggedInUser = {}
 updateApp();
