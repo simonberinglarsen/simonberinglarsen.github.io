774b4ae440034bf4c401e28a926abab38d5f1264
Simon Bering Larsen <XSISE@orsted.dk>
2020-08-13 14:19:50 +0200

    .

--- membermanager/main.js
+++ membermanager/main.js
@@ -1,3 +1,4 @@
+// global array defining all admin users
 const admins = [
     {
         adminID: "a001",
@@ -6,6 +7,7 @@ const admins = [
     },
 ];
 
+// global array defining all members
 const members = [
     {
         memID: "m001",
@@ -26,15 +28,17 @@ const members = [
 
 ];
 
+// application state - keeps track of things we save accross screens
 const appState = {
     loggedInUser: null,
-
 };
 
+// toggle whether or not to show a specific html element (given by id)
 function isVisible(id, show) {
     document.getElementById(id).setAttribute("style", show ? "" : "display:none")
 }
 
+// renders the member table in html based on the javascript arrays
 function renderUserTable() {
     const mapToRow  = (member) => `<tr>   
     <th>${member.memID}</th>
@@ -45,25 +49,29 @@ function renderUserTable() {
     <td>${member.memAge}</td></tr>`;
 
     document.getElementById('userTable').tBodies[0].innerHTML = `${members.map(m => mapToRow(m)).join('')}`;
-    
 }
 
+// updates the application - keeps track of what to show and what to hide. Also updating dynamic things based on application state (appState)
 function updateApp() {
     isVisible('loginScreen', !appState.loggedInUser);
     isVisible('loginValidation', appState.invalidUser);
     isVisible('memberAdminScreen', appState.loggedInUser);
     if (appState.loggedInUser) {
+        document.getElementById('btnLogout').innerText = `Logout (${appState.loggedInUser.adminID})`;
         renderUserTable();
     }
 
 }
 
+// button to logout admin user. this method updates the state and calls the updateapp method to reflect changes
 document.getElementById('btnLogout').onclick = function () {
     appState.loggedInUser = null;
     appState.invalidUser = false;
     updateApp();
 }
 
+// button to login. gets the username and password from the text boxes and see if there is a matching user in the global array.
+// in the end the application state is updated and the updateApp() method makes sure to hide/show the right stuff
 document.getElementById('btnLogin').onclick = function () {
     let user = document.getElementById('loginUser').value;
     let pass = document.getElementById('loginPassword').value;
@@ -78,5 +86,5 @@ document.getElementById('btnLogin').onclick = function () {
     updateApp();
 }
 
-appState.loggedInUser = {}
+// this updates the initial state of the app.
 updateApp();
