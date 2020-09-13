961f985ec6124fd7091e806d3293dd26734fa283
Simon Larsen <sendsla@gmail.com>
2020-09-13 11:26:10 +0200

    .

--- landingpage/app.js
+++ landingpage/app.js
@@ -1,1 +1,18 @@
 $('#test').html('hello')
+
+const description = `cloud architect and site reliability engineer with over a decade of intense professional experience strictly adhering to DevOps methodology. He has architected and built multiple platform-agnostic infrastructures from scratch for modern cloud systems`;
+
+new TypeIt("#header-description", {
+    strings: "",
+    speed: 20,
+    lifeLike: true,
+    cursor: true,
+    waitUntilVisible: true,
+    afterComplete: async (step, instance) => {
+        instance.destroy();
+    }
+})
+.type(description, {delay: 1000})
+.go();
+
+
